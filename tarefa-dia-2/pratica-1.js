9a573d9c40850b869c2c8ffd3b53017dc370fac6
Sylvinha <sylviazonari@gmail.com>
2022-08-24 15:12:12 -0300

    mensagem de commit

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,11 @@ let email = "sylviazonari@gmail.com"
 let profissao = "corretora de imoveis"
 let idade = 36
 let lugarPerfeito = "Bali"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugarPerfeito)
+    
+
+
