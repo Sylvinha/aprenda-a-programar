11a44a245bf13658db156b53c4b95d2b23659936
Sylvinha <sylviazonari@gmail.com>
2022-08-27 15:44:21 -0300

    tarefa-dia-3

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,12 @@
+// fazendo a tabuada de 3
+
+let multiplicador = 3
+let n = 10  // multiplicar de 1 a n
+let i = 1  // i = contador
+
+while (i<= n){
+    console.log(`${multiplicador} * ${i} = ${multiplicador * i}`)
+    i ++
+}
+
+
