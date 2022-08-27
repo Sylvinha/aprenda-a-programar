d0d40070868e8bccda4588ea60e5fe58ce3cbf54
Sylvinha <sylviazonari@gmail.com>
2022-08-27 16:51:03 -0300

    tarefa-dia-3

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,68 @@
+// números primos
+
+let numero = 17
+// operador resto: %
+// saber se é primo ou não
+//faz o resto por 1. se o resto for 0 , significa que ele é divisível. 
+
+if (numero % 1 == 0){
+    console.log(`Sim, o número ${numero} é primo!`)
+} else{
+    console.log(`Não, o número ${numero} não é primo`)
+}
+
+let numero1 = 2 
+
+if (numero1 % 1 == 0){
+    console.log(`Sim, o número ${numero1} é primo!`)
+} else{
+    console.log(`Não, o número ${numero1} não é primo`)
+}
+
+let numero2 = 7
+
+if (numero2 % 1 == 0){
+    console.log(`Sim, o número ${numero2} é primo!`)
+} else{
+    console.log(`Não, o número ${numero2} não é primo`)
+}
+
+let numero3 = 8
+
+if (numero3 % 3 == 0){
+    console.log(`Sim, o número ${numero3} é primo!`)
+} else{
+    console.log(`Não, o número ${numero3} não é primo`)
+}
+
+let numero4 = 5
+
+if (numero4 % 1 == 0){
+    console.log(`Sim, o número ${numero4} é primo!`)
+} else{
+    console.log(`Não, o número ${numero4} não é primo`)
+}
+
+let numero5 = 23
+
+if (numero5 % 1 == 0){
+    console.log(`Sim, o número ${numero5} é primo!`)
+} else{
+    console.log(`Não, o número ${numero5} não é primo`)
+}
+
+let numero6 = 15
+
+if (numero6 % 2 == 0){
+    console.log(`Sim, o número ${numero6} é primo!`)
+} else{
+    console.log(`Não, o número ${numero6} não é primo`)
+}
+
+let numero7 = 1500450271
+
+if (numero7 % 1 == 0){
+    console.log(`Sim, o número ${numero7} é primo!`)
+} else{
+    console.log(`Não, o número ${numero7} não é primo`)
+}
